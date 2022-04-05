5f76f176192e5ecf816031717c69dc2816727a1c
Alicia <davoyaaj@mail.uc.edu>
2022-04-05 01:52:14 -0400

    It was missing the JRE System Library so I added it, at least on my end.
    I then removed the - 1 from r.nextInt for both UPCs and flavors in the
    for loop in the add method.

--- TwoLiterStack/src/main/TwoLiter.java
+++ TwoLiterStack/src/main/TwoLiter.java
@@ -9,7 +9,7 @@
  * Description: This is our 10th assignment and we use our debugging skillz to fix the program and figure out why there is no Dr. Pepper and fix it.
  * Anything else: 
  */
-
+ 
 package main;
 
 import java.util.Random;
